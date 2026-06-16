a3a9ebd19f3240900e200d5e3ae476164748e91d
Anthony140823 <anthonyreyes140803@gmail.com>
2026-06-16 16:14:46 -0500

    Creación del main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+        CALCULADORA (CON FUNCIONES)
+
+Crear un programa para agregar
+las operaciones básicas de una calucladora.
+
+Las operaciones que debe poder realizar son:
+1. Sumar
+2. Restar
+3. Multiplicar
+4. Dividir
+
+Se debe agregar un menú para mostrar
+cada opción.
+'''
+# Pograma principal
+if __name__ == "__main__":
+    print("*** Calculadora con Funciones ***")
+    
